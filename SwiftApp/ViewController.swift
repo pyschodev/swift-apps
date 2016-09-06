3f03ddedf2ba7cf83ec5cb7dd778b11c568b36f7
Chawn <lfinley05@gmail.com>
2016-09-05 23:16:11 -0500

    first commit

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Chawn on 9/5/16.
+//  Copyright © 2016 ChawnFinley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
